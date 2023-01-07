3fd86ddd48c3d739f780bfb7da9185ff1613cd2c
Yerin Jung <yell_10@naver.com>
2023-01-07 17:16:19 +0900

    [기초-산술연산] 정수 2개 입력받아 나눈 나머지 계산하기 (23.01.07)

---
+++ [기초-산술연산] 정수 2개 입력받아 나눈 나머지 계산하기.py
@@ -0,0 +1,8 @@
+# [기초-산술연산] 정수 2개 입력받아 나눈 나머지 계산하기
+
+# 나눈 나머지를 계산하는 연산자(%, remainder)
+# a%b : a를 b로 나눈 나머지(remainder), 나머지 연산(modulus, mod 연산)
+
+a, b = input().split(' ')
+
+print(int(a) % int(b))
