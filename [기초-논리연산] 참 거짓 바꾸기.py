d77020b6573908e2f986a1962e034a70c500d35b
Yerin Jung <yell_10@naver.com>
2023-01-08 14:35:36 +0900

    [기초-논리연산] 참 거짓 바꾸기 (23.01.08)

---
+++ [기초-논리연산] 참 거짓 바꾸기.py
@@ -0,0 +1,8 @@
+# [기초-논리연산] 참 거짓 바꾸기
+
+# 참 또는 거짓의 논리값을 역(반대)으로 바꾸기 위해서 not 예약어(reserved word, keyword)를 사용
+# 참, 거짓의 논리값 인 불(boolean) 값을 다루어주는 예약어는 not, and, or
+
+num = bool(int(input()))
+
+print(not num)
