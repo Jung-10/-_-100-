98ed0cb1b4a18a615b811a3c1eb917009874c0aa
Yerin Jung <yell_10@naver.com>
2023-01-10 22:26:18 +0900

    [기초-조건/선택실행구조] 점수 입력받아 평가 출력하기 (23.01.10)

---
+++ [기초-조건,선택실행구조] 점수 입력받아 평가 출력하기.py
@@ -0,0 +1,16 @@
+# [기초-조건/선택실행구조] 점수 입력받아 평가 출력하기
+
+score = int(input())
+
+if 90 <= score <= 100 :
+    print('A')
+
+elif 70 <= score <= 89 :
+    print('B')
+
+elif 40 <= score <= 69 :
+    print('C')
+
+elif 0 <= score <= 39 :
+    print('D')
+
