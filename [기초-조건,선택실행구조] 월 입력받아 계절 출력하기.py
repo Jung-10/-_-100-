6486850964eb6b48bfa1e55da7321f5606d4c719
Yerin Jung <yell_10@naver.com>
2023-01-10 22:32:40 +0900

    [기초-조건/선택실행구조] 월 입력받아 계절 출력하기 (23.01.10)

---
+++ [기초-조건,선택실행구조] 월 입력받아 계절 출력하기.py
@@ -0,0 +1,15 @@
+# [기초-조건/선택실행구조] 월 입력받아 계절 출력하기
+
+num = int(input())
+
+if num // 3 == 1 :
+    print('spring')
+
+elif num // 3 == 2 :
+    print('summer')
+
+elif num // 3 == 3 :
+    print('fall')
+
+else :
+    print('winter')
